ea3654ceed53049391d6b125cc6388c7ccc3c301
Ivan Orsolic <orsolic1995@gmail.com>
2024-09-17 15:56:52 +0200

    Update constants.ts

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,7 +1,7 @@
 export const INSTANT_SEARCH_INDEX_NAME = 'poc_alt_HAAS';
 export const INSTANT_SEARCH_QUERY_SUGGESTIONS = 'poc_alt_HAAS_query_suggestions';
 export const INSTANT_SEARCH_HIERARCHICAL_ATTRIBUTES = [
-  'parent_categories.lvl0',
+  'Category',
   'parent_categories.lvl1',
 ];
 export const APP_ID = 'YPVLAW1B55';
