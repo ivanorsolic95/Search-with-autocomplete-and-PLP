3d86845a0a1b4cc33c72c48f9943d32b7c65845f
Ivan Orsolic <orsolic1995@gmail.com>
2024-09-17 12:19:32 +0200

    Fix issue with saving recent search term
    The search term is not saved in search history when user clicks on product from search suggestion.
    Add handleClick to manually save search term.

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,30 +1,36 @@
 import React from 'react';
 import ProductHit from '../types.tsx/ProductHit';
-import '../css/globals.css'
 
-const ProductItem: React.FC<{ hit: ProductHit, components: any }> = ({ hit, components }) => {
-    const lastParentCategory = hit.parent_categories ? Object.values(hit.parent_categories).pop() : '';
+const ProductItem: React.FC<{ hit: ProductHit, components: any, query: string, addItem: (item: any) => void }> = ({ hit, components, addItem, query }) => {
+  const lastParentCategory = hit.parent_categories ? Object.values(hit.parent_categories).pop() : '';
+
+  const handleClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    // Handle left-click (button === 0) or middle-click (button === 1) or right-click (button === 2)
+    if (event.button === 0 || event.button === 1 || event.button === 2) {
+      // Save the recent search term manually
+      addItem({id:query ,label: query });
+    }
+  };
   
-    return (
-      <a href={hit.url} className="aa-ItemWrapper">
-        <div className="aa-ItemContent">
-          <div className="aa-ItemIcon aa-ItemIcon--picture aa-ItemIcon--alignTop">
-            <img src={hit.image} alt={hit.name} width="40" height="40" />
+  return (
+    <a href={hit.url} className="aa-ItemWrapper" onMouseDown={handleClick}>
+      <div className="aa-ItemContent">
+        <div className="aa-ItemIcon aa-ItemIcon--picture aa-ItemIcon--alignTop">
+          <img src={hit.image} alt={hit.name} width="40" height="40" />
+        </div>
+        <div className="aa-ItemContentBody">
+          <div className="aa-ItemContentTitle">
+            <components.Highlight hit={hit} attribute="name" />
           </div>
-          <div className="aa-ItemContentBody">
-            <div className="aa-ItemContentTitle">
-              <components.Highlight hit={hit} attribute="name" />
-            </div>
-            <div className="aa-ItemContentDescription">
-              <div>In <strong>{lastParentCategory}</strong></div>
-              <div>SKU: {hit.sku}</div>
-              <div>${hit.price}</div>
-            </div>
+          <div className="aa-ItemContentDescription">
+            <div>In <strong>{lastParentCategory}</strong></div>
+            <div>SKU: {hit.sku}</div>
+            <div>${hit.price}</div>
           </div>
         </div>
-      </a>
-    );
+      </div>
+    </a>
+  );
 };
 
 export default ProductItem;
-  
